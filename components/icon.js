745cb0bad512e753d6da8ceaf4ffbd03f82f0204
ilamos <ilmonen.amos@gmail.com>
2022-11-23 05:25:45 +0200

    Improved icon component
    ~ Size is now just a number argument
    ~ Fixed invert for fast refresh

--- components/icon.js
+++ components/icon.js
@@ -2,23 +2,19 @@ import Image from "next/image";
 import styles from '../styles/Home.module.css'
 import {useEffect, useState} from "react";
 
-const size_defs = {
-    small: 10,
-    medium: 20,
-    large: 30,
-}
-
 export default function Icon({name, size, inverted}) {
     const [class_name, class_name_update] = useState(styles.icon);
     useEffect(() => {
         if (inverted) {
             class_name_update(styles.icon_inverted);
+        } else {
+            class_name_update(styles.icon); // Fix for fast refresh
         }
     })
 
     return (
         <>
-            <Image className={class_name} src={`/images/icons/${name}.svg`} alt={`Icon: ${name}`} width={size_defs[size]} height={size_defs[size]} />
+            <Image className={class_name} src={`/images/icons/${name}.svg`} alt={`Icon: ${name}`} width={size} height={size} />
         </>
     )
 }
